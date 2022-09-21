f1ceb08ec9e4277f4521d4c4d248102ef80636a9
Patrick Mota <58093259+patrickmps@users.noreply.github.com>
2022-09-20 22:05:56 -0300

    Rotas e navegação

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import Profile from "./pages/Profile";
+import User from "./pages/User";
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/" element={<User/>}/>
+                <Route path="/create" element={<Profile/>}/>
+                <Route path="/update/:id" element={<Profile/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
+}
